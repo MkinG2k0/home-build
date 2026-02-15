acb89b1176688243fb36b8a3c8506d1c1bff8a7f
mk <kamai122000@mail.ru>
2026-02-15 21:37:58 +0300

    init

--- src/app/AppShell.tsx
+++ src/app/AppShell.tsx
@@ -1,33 +1,33 @@
-import { IonRouterOutlet, IonSplitPane } from '@ionic/react';
-import * as React from 'react';
-import { Route } from 'react-router-dom';
+import { IonRouterOutlet, IonSplitPane } from '@ionic/react'
+import * as React from 'react'
+import { Route } from 'react-router-dom'
 
-import { AppHeader } from '../widgets/app-header';
-import { AppMenu } from '../widgets/app-menu';
-import { CallFab } from '../widgets/call-fab';
-import { AboutPage } from '../pages/about';
-import { CatalogPage } from '../pages/catalog';
-import { HomePage } from '../pages/home';
-import { NewsPage } from '../pages/news';
-import { VideoblogPage } from '../pages/videoblog';
-
-const MAIN_ID = 'main';
+import { AppHeader } from '../widgets/app-header'
+import { AppMenu } from '../widgets/app-menu'
+import { CallFab } from '../widgets/call-fab'
+import { AboutPage } from '../pages/about'
+import { CatalogPage } from '../pages/catalog'
+import { HomePage } from '../pages/home'
+import { NewsPage } from '../pages/news'
+import { VideoblogPage } from '../pages/videoblog'
+	
+const MAIN_ID = 'main'
 
 export const AppShell: React.FC = () => (
-  <IonSplitPane contentId={MAIN_ID}>
-    <AppMenu contentId={MAIN_ID} />
-    <div className="flex flex-1 flex-col bg-slate-50" id={MAIN_ID}>
-      <AppHeader />
-      <main className="relative flex-1 overflow-auto bg-slate-50">
-        <IonRouterOutlet id="router-outlet">
-          <Route component={HomePage} exact path="/" />
-          <Route component={CatalogPage} path="/catalog" />
-          <Route component={NewsPage} path="/news" />
-          <Route component={VideoblogPage} path="/videos" />
-          <Route component={AboutPage} path="/about" />
-        </IonRouterOutlet>
-      </main>
-      <CallFab />
-    </div>
-  </IonSplitPane>
-);
+	<IonSplitPane contentId={MAIN_ID}>
+		<AppMenu contentId={MAIN_ID}/>
+		<div className="flex flex-1 flex-col bg-slate-50" id={MAIN_ID}>
+			<AppHeader/>
+			<main className="relative flex-1 overflow-auto bg-slate-50">
+				<IonRouterOutlet id="router-outlet">
+					<Route component={HomePage} exact path="/"/>
+					<Route component={CatalogPage} path="/catalog"/>
+					<Route component={NewsPage} path="/news"/>
+					<Route component={VideoblogPage} path="/videos"/>
+					<Route component={AboutPage} path="/about"/>
+				</IonRouterOutlet>
+			</main>
+			<CallFab/>
+		</div>
+	</IonSplitPane>
+)
