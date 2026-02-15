77e5ec77da9f274c4657ee8b2a713c468be937a6
mk <kamai122000@mail.ru>
2026-02-15 15:23:28 +0300

    init

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -14,14 +14,17 @@ const Page: React.FC = () => {
 					<IonButtons slot="start">
 						<IonMenuButton/>
 					</IonButtons>
-					<IonTitle>{name}</IonTitle>
+					<IonTitle>{name}
+					</IonTitle>
 				</IonToolbar>
 			</IonHeader>
 
 			<IonContent fullscreen>
 				<IonHeader collapse="condense">
 					<IonToolbar>
-						<IonTitle size="large">{name}</IonTitle>
+
+						<IonTitle size="large">{name}
+						</IonTitle>
 					</IonToolbar>
 				</IonHeader>
 				<ExploreContainer name={name}/>
